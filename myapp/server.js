0a8c5c95b16cc21125cde1103e5e80f6ca8a0394
Rakesh Choudhary <rakeshjunjadia0007@gmail.com>
2025-03-20 12:51:33 +0530

    Updated server message for auto-deploy test

--- myapp/server.js
+++ myapp/server.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('🚀 Dummy App is Running!'));
+app.get('/', (req, res) => res.send('🚀 Dummy App is sucess!'));
 app.listen(3000, () => console.log('Server started on port 3000'));
