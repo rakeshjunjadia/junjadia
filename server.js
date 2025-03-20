40bac95977dc903e75bed261308c39fdbbb8c864
Rakesh Choudhary <rakeshjunjadia0007@gmail.com>
2025-03-20 15:42:12 +0530

    Updated code

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('🚀 Dummy App is rakesh!'));
+app.get('/', (req, res) => res.send('🚀 Dummy App is lokesh!'));
 app.listen(3000, () => console.log('Server started on port 3000'));
